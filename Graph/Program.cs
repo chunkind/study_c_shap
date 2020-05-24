a0fdd2b4834d1cb301ce353a141446eb2dcaf43c
chunkind <chunkind88@gmail.com>
2020-05-24 17:36:50 +0900

    커밋

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -3,48 +3,31 @@ using System.Collections.Generic;
 
 namespace Graph
 {
-    class Graph
-    {
-        int[,] adj = new int[6, 6]
-        {
-            { 0, 1, 0, 1, 0, 0 },
-            { 1, 0, 1, 1, 0, 0 },
-            { 0, 1, 0, 0, 0, 0 },
-            { 1, 1, 0, 0, 1, 0 },
-            { 0, 0, 0, 1, 0, 1 },
-            { 0, 0, 0, 0, 1, 0 },
-        };
-
-        List<int>[] adj2 = new List<int>[]
-        {
-            new List<int>() { 1, 3 },
-            new List<int>() { 0, 2, 3 },
-            new List<int>() { 1 },
-            new List<int>() { 0, 1, 4 },
-            new List<int>() { 3, 5 },
-            new List<int>() { 4 },
-        };
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             //리스트 순회
-            List<int> list = new List<int>() { 1, 2, 3, 4 };
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.WriteLine(list[i]);
-            }
-            foreach(int val in list)
-            {
-                Console.WriteLine(val);
-            }
+            //List<int> list = new List<int>() { 1, 2, 3, 4 };
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    Console.WriteLine(list[i]);
+            //}
+            //foreach (int val in list)
+            //{
+            //    Console.WriteLine(val);
+            //}
 
             //그래프 순회
             // DFS(Depth First Search : 깊이 우선 탐색)
+            //ExDFS dfs = new ExDFS();
+            //dfs.DFS(3);
+            //dfs.DFS2(3);
+            //dfs.SearchAll();
 
             // BFS(Breadth First Search : 너비 우선 탐색)
+            ExBFS bfs = new ExBFS();
+            bfs.BFS(0);
 
         }
     }
